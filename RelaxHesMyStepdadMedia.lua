c6759e5bc6a0412e4dabebabfd8e183156290210
Kolby Kruger <kolbykruger@gmail.com>
2024-10-09 08:52:14 -0400

    Init + initial file dump

---
+++ RelaxHesMyStepdadMedia.lua
@@ -0,0 +1,33 @@
+local LSM = LibStub("LibSharedMedia-3.0")
+
+--StatusBars
+LSM:Register("statusbar","Stepdad", [[Interface\Addons\RelaxHesMyStepdadMedia\StatusBars\A1.tga]])
+LSM:Register("statusbar","Stepdad Textured", [[Interface\Addons\RelaxHesMyStepdadMedia\StatusBars\A1Texture.tga]])
+LSM:Register("statusbar","Stepdad Striped", [[Interface\Addons\RelaxHesMyStepdadMedia\StatusBars\Striped.tga]])
+
+--Sounds
+LSM:Register("sound", "|cFFe35ab7|r", [[Interface\Addons\RelaxHesMyStepdadMedia\Sounds\.mp3]])
+LSM:Register("sound", "|cFFe35ab7Alarm|r", [[Interface\Addons\RelaxHesMyStepdadMedia\Sounds\alarm.mp3]])
+LSM:Register("sound", "|cFFe35ab7Alert|r", [[Interface\Addons\RelaxHesMyStepdadMedia\Sounds\alert.mp3]])
+LSM:Register("sound", "|cFFe35ab7Alert 2|r", [[Interface\Addons\RelaxHesMyStepdadMedia\Sounds\alert2.mp3]])
+LSM:Register("sound", "|cFFe35ab7Alert 3|r", [[Interface\Addons\RelaxHesMyStepdadMedia\Sounds\alert3.mp3]])
+LSM:Register("sound", "|cFFe35ab7Bong|r", [[Interface\Addons\RelaxHesMyStepdadMedia\Sounds\bong.mp3]])
+LSM:Register("sound", "|cFFe35ab7Censor|r", [[Interface\Addons\RelaxHesMyStepdadMedia\Sounds\censor.mp3]])
+LSM:Register("sound", "|cFFe35ab7Derka|r", [[Interface\Addons\RelaxHesMyStepdadMedia\Sounds\derkaderkaderka.mp3]])
+LSM:Register("sound", "|cFFe35ab7Ding|r", [[Interface\Addons\RelaxHesMyStepdadMedia\Sounds\ding.mp3]])
+LSM:Register("sound", "|cFFe35ab7Dispel|r", [[Interface\Addons\RelaxHesMyStepdadMedia\Sounds\dispel.mp3]])
+LSM:Register("sound", "|cFFe35ab7External|r", [[Interface\Addons\RelaxHesMyStepdadMedia\Sounds\external.mp3]])
+LSM:Register("sound", "|cFFe35ab7Gnome|r", [[Interface\Addons\RelaxHesMyStepdadMedia\Sounds\gnome.mp3]])
+LSM:Register("sound", "|cFFe35ab7Gromnack - Get Me Out|r", [[Interface\Addons\RelaxHesMyStepdadMedia\Sounds\getmeout.mp3]])
+LSM:Register("sound", "|cFFe35ab7Gromnack - Im Done|r", [[Interface\Addons\RelaxHesMyStepdadMedia\Sounds\imdone.mp3]])
+LSM:Register("sound", "|cFFe35ab7Gromnack - Huh|r", [[Interface\Addons\RelaxHesMyStepdadMedia\Sounds\huh.mp3]])
+LSM:Register("sound", "|cFFe35ab7Interrupt|r", [[Interface\Addons\RelaxHesMyStepdadMedia\Sounds\interrupt.mp3]])
+LSM:Register("sound", "|cFFe35ab7Nut|r", [[Interface\Addons\RelaxHesMyStepdadMedia\Sounds\nut.mp3]])
+LSM:Register("sound", "|cFFe35ab7Pedro|r", [[Interface\Addons\RelaxHesMyStepdadMedia\Sounds\pedrolust.mp3]])
+LSM:Register("sound", "|cFFe35ab7Merica Pull Timer|r", [[Interface\Addons\RelaxHesMyStepdadMedia\Sounds\pullamerica15.mp3]])
+LSM:Register("sound", "|cFFe35ab7Tim Allen|r", [[Interface\Addons\RelaxHesMyStepdadMedia\Sounds\timallen.mp3]])
+LSM:Register("sound", "|cFFe35ab7Yeyeah|r", [[Interface\Addons\RelaxHesMyStepdadMedia\Sounds\yeyeah.mp3]])
+
+--Fonts
+LSM:Register("font","Expressway", [[Interface\Addons\RelaxHesMyStepdadMedia\Fonts\Expressway.TTF]])
+LSM:Register("font","Naowh", [[Interface\Addons\RelaxHesMyStepdadMedia\Fonts\Naowh.TTF]])
